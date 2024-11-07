b363e13e6fb6bb0b6ddf01ba6f8f2d3bad27076f
Bushra Kidanemariam <bkm8185@gmail.com>
2024-11-07 16:02:59 +0300

    Create AES.java

---
+++ src/main/java/crypto/AES.java
@@ -0,0 +1,44 @@
+
+### src/main/java/crypto/AES.java
+
+```java
+/**
+ *
+ * @author Bushra
+ */
+package crypto;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import java.util.Base64;
+
+public class AES {
+    public static String encrypt(String data, SecretKey key) throws Exception {
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.ENCRYPT_MODE, key);
+        byte[] encryptedData = cipher.doFinal(data.getBytes());
+        return Base64.getEncoder().encodeToString(encryptedData);
+    }
+
+    public static String decrypt(String encryptedData, SecretKey key) throws Exception {
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.DECRYPT_MODE, key);
+        byte[] decodedData = Base64.getDecoder().decode(encryptedData);
+        byte[] decryptedData = cipher.doFinal(decodedData);
+        return new String(decryptedData);
+    }
+
+    public static void main(String[] args) throws Exception {
+        KeyGenerator keyGen = KeyGenerator.getInstance("AES");
+        keyGen.init(128);
+        SecretKey key = keyGen.generateKey();
+
+        String data = "Hello, World!";
+        String encryptedData = encrypt(data, key);
+        System.out.println("Encrypted: " + encryptedData);
+
+        String decryptedData = decrypt(encryptedData, key);
+        System.out.println("Decrypted: " + decryptedData);
+    }
+}
