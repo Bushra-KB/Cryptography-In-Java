70d7a1b598b2cbd18fb56795647d5409024163d3
Bushra Kidanemariam <bkm8185@gmail.com>
2024-11-07 16:06:52 +0300

    Update TripleDES.java

--- src/main/java/crypto/TripleDES.java
+++ src/main/java/crypto/TripleDES.java
@@ -12,4 +12,29 @@ import java.util.Base64;
 public class TripleDES {
     public static String encrypt(String data, SecretKey key) throws Exception {
         Cipher cipher = Cipher.getInstance("DESede");
-        cipher.init(Cipher.ENCRYPT
+        cipher.init(Cipher.ENCRYPT_MODE, key);
+        byte[] encryptedData = cipher.doFinal(data.getBytes());
+        return Base64.getEncoder().encodeToString(encryptedData);
+    }
+
+    public static String decrypt(String encryptedData, SecretKey key) throws Exception {
+        Cipher cipher = Cipher.getInstance("DESede");
+        cipher.init(Cipher.DECRYPT_MODE, key);
+        byte[] decodedData = Base64.getDecoder().decode(encryptedData);
+        byte[] decryptedData = cipher.doFinal(decodedData);
+        return new String(decryptedData);
+    }
+
+    public static void main(String[] args) throws Exception {
+        KeyGenerator keyGen = KeyGenerator.getInstance("DESede");
+        keyGen.init(168);
+        SecretKey key = keyGen.generateKey();
+
+        String data = "Hello, World!";
+        String encryptedData = encrypt(data, key);
+        System.out.println("Encrypted: " + encryptedData);
+
+        String decryptedData = decrypt(encryptedData, key);
+        System.out.println("Decrypted: " + decryptedData);
+    }
+}
